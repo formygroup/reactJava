b329ac9428f6721a2d981c16c73a9713e08737df
a304258590 <304258590@qq.com>
2020-07-31 15:06:11 +0800

    initial commit

---
+++ src/Components/Test1.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+import style from '../assets/css/Test1.module.css'
+class Test1 extends Component {
+    render() {
+        return (
+            <div>
+                <h2 className={style.red}>test1</h2>
+            </div>
+        )
+    }
+}
+
+export default Test1
